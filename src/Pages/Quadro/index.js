456acf208328e32af0ce9c08230187cb4a6fbd10
Adler Cachuba <adlercachuba@gmail.com>
2021-11-15 15:14:02 -0300

    Consumindo a rota get para mostrar o nome dos quadros, e imagem dos quadros, assim como funcionando o click na imagem para exibir o nome.

---
+++ src/Pages/Quadro/index.js
@@ -0,0 +1,41 @@
+import React, {Component} from "react";
+import {View,Text,Image,StyleSheet, TouchableOpacity} from 'react-native';
+import {Touchable} from "react-native-web";
+
+export default class Quadro extends Component{
+    render(){
+        return(
+            <View>
+                <Text style={styles.nomeQuadro}>{this.props.data.nome} </Text>
+                {/*função anonima que ao clicque faz a ação*/}
+                <TouchableOpacity onPress={()=> alert(this.props.data.nome)}>
+                    <Image style={styles.fotoQuadro} source={{uri: this.props.data.foto}}></Image>
+                </TouchableOpacity>
+
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+   container:{
+
+   },
+   nomeQuadro:{
+       color:'white',
+       fontSize:22,
+       fontWeight:'bold',
+       justifyContent:'center',
+       alignSelf:'center',
+       marginTop: 30,
+       marginBottom:20,
+
+   },
+
+    fotoQuadro:{
+       width: 200,
+        height:250,
+        borderRadius:30,
+        alignSelf:'center',
+    }
+})
